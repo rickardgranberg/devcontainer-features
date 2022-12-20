99d854fec22735e6de8fce7966af31c8aeed6255
Rickard Granberg <rickard.granberg@infovista.com>
2022-12-20 14:09:21 +0000

    Initial version

---
+++ src/oh-my-posh/README.md
@@ -0,0 +1,28 @@
+
+# Oh-my-posh (oh-my-posh)
+
+Installs [https://ohmyposh.dev/](https://ohmyposh.dev/)
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/rickardgranberg/devcontainer-features/oh-my-posh": {
+        "version": "latest",
+        "themeURL": "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/agnoster.omp.json"
+    }
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+| version | Select the oh-my-posh version | string | latest |
+| themeURL| URL to the theme to use | string | https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/jandedobbeleer.omp.json |
+
+
+
+---
+
+_Note: This file was auto-generated from the [devcontainer-feature.json](https://github.com/devcontainers/feature-starter/blob/main/src/hello/devcontainer-feature.json).  Add additional notes to a `NOTES.md`._
---
+++ src/oh-my-posh/install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+echo "Activating feature 'oh-my-posh'"
+
+version=${VERSION:-latest}
+themeURL=${THEMEURL:-"https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/jandedobbeleer.omp.json"}
+themeName=${themeURL##*/}
+themeTarget="/home/${_REMOTE_USER}/.poshthemes/${themeName}"
+
+arch=$(uname -m)
+if [[ $arch == 'x86_64' ]]; then
+    arch='amd64'
+elif [[ $arch == 'aarch64' ]]; then
+    arch='arm64'
+fi
+
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/${VERSION}/download/posh-linux-${arch} -O /usr/local/bin/oh-my-posh
+chmod +x /usr/local/bin/oh-my-posh
+
+mkdir -p /home/${_REMOTE_USER}/.poshthemes
+wget ${themeURL} -O ${themeTarget}
+chown ${_REMOTE_USER}:${_REMOTE_USER} ${themeTarget}
+
+# zsh
+zshSnippet="eval \"\$(oh-my-posh init zsh --config '${themeTarget}')\""
+
+# bash
+bashSnippet="eval \"\$(oh-my-posh init bash --config '${themeTarget}')\""
+
+echo "${zshSnippet}" | tee -a /root/.zshrc >> /home/${_REMOTE_USER}/.zshrc
+echo "${bashSnippet}" | tee -a /root/.bashrc >> /home/${_REMOTE_USER}/.bashrc
