f1a7fe3d587dfc89fe637574b75a20a7f6a47a36
Rickard Granberg <rickard.granberg@infovista.com>
2023-05-15 06:20:06 +0000

    Fix ARM arch

--- src/protoc/install.sh
+++ src/protoc/install.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 PROTOC_VER=${VERSION:-"23.0"}
-ARCH="$(uname -m)"
+arch="$(uname -m)"
+
+if [[ $arch == 'x86_64' ]]; then
+    arch='x86_64'
+elif [[ $arch == 'aarch64' ]]; then
+    arch='aarch_64'
+fi
+
 # Install proto compiler
 
 has_wget=$(which wget)
@@ -12,11 +19,11 @@ if [[ -z "$has_wget" ]]; then
     apt-get -y install --no-install-recommends wget ca-certificates unzip
 fi
 
-wget -q https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VER}/protoc-${PROTOC_VER}-linux-${ARCH}.zip
-unzip -qq protoc-${PROTOC_VER}-linux-${ARCH}.zip
+wget -q https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VER}/protoc-${PROTOC_VER}-linux-${arc}.zip
+unzip -qq protoc-${PROTOC_VER}-linux-${arch}.zip
 mv bin/protoc /usr/local/bin/
 chmod 0755 /usr/local/bin/protoc
 mv include/google /usr/local/include/
 chmod -R a+rX /usr/local/include/google
-rm -f protoc-${PROTOC_VER}-linux-${ARCH}.zip 
+rm -f protoc-${PROTOC_VER}-linux-${arch}.zip 
 
